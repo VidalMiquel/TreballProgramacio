b92f4a8c25ca124df0cdbf0625a153d88f4c04bc
MiquelVidalCortes <miquelvidalcortes@gmail.com>
2021-01-09 01:04:13 +0100

    CodificacioFeta

---
+++ src/projecteprogramacio/CodificacionAlfabetica.java
@@ -0,0 +1,63 @@
+package projecteprogramacio;
+
+import static java.lang.Math.abs;
+import java.util.Random;
+
+public class CodificacionAlfabetica {
+
+    private static final int NUMERO_CARACTERES=27;
+    private static final char [] ALFABETO="abcdefghijklmnñopqrstuvwxyz".toCharArray();
+    private int desplazamiento;
+    
+
+    public CodificacionAlfabetica(int semilla) {
+        Random generador=new Random(semilla*1000);
+        desplazamiento=(int) (generador.nextDouble()*NUMERO_CARACTERES);
+    }
+    
+    //MÉTODO codificar LLEVA A TÉRMINO LA OBTENCIÓN DEL CARACTER CODIFICADO
+    //DEL CARACTER DADO POR PARÁMETRO
+    public char codificar(char caracter) {
+        if (valido(caracter)) {
+            return ALFABETO[(posicion(caracter)+desplazamiento)%NUMERO_CARACTERES];
+        }
+        else {
+            return caracter;
+        }
+        
+    }
+
+    //MÉTODOS FUNCIONALES
+    //MÉTODO deCodificar LLEVA A TÉRMINO LA OBTENCIÓN DEL CARACTER DECODIFICADO
+    //DEL CARACTER DADO POR PARÁMETRO
+    public char deCodificar(char caracter) {
+        if (valido(caracter)) {
+            int valor=posicion(caracter)-desplazamiento;
+            if (valor<0) {
+                valor=NUMERO_CARACTERES-abs(valor);
+            }
+            return ALFABETO[valor%NUMERO_CARACTERES];            
+        }
+        else {
+            return caracter;
+        }
+    }    
+    //MÉTODO posicion OBTIENE LA POSICIÓN EN EL ARRAY ALFABETO DE UN CARACTER
+    //DADO POR PARÁMETRO
+    public int posicion(char caracter) {
+        int indice;
+        for (indice=0;caracter!=ALFABETO[indice];indice++) {}
+        return indice;
+    }
+    
+    //MÉTODO valido VERIFICA SI UN CARACTER DADO ES UN CARACTER QUE PUEDE
+    //SER CODIFICADO O DECODIFICADO
+    private boolean valido(char caracter) {
+        for (int indice=0;indice<ALFABETO.length;indice++) {
+            if (caracter==ALFABETO[indice]) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
--- src/projecteprogramacio/Linia.java
+++ src/projecteprogramacio/Linia.java
@@ -3,41 +3,89 @@ package projecteprogramacio;
 
 public class Linia {
     
-    int numeroCaracteres;
-    private static final int NUMERO_MAXIMO_CARACTERES = 250;
-    char [] caracteres = new char [NUMERO_MAXIMO_CARACTERES];
+     //DECLARACIONES
+    //Declaración atributo de clase constante entero que representa el final de
+    //un fichero
+    private static final char FINAL_LINEA='\n';
+    //declaración atributo de clase constante entero que representa el máximo
+    //número de caracteres que puede contener un objetoLinea
+    private static final int MAXIMO=250;
+    //declaración atributo de objeto variable array de caracteres que representa 
+    //los caracteres de un objeto Linea 
+    private char [] caracteres=new char[MAXIMO];  
+    //declaración atributo de objeto variable entero que represente el número
+    //de caracteres de un objeto Linea
+    private int numeroCaracteres;   
+    //declaración atributo de clase variable caracter que representa el último
+    //caracter leido desde la secuencia de entrada por teclado
+    private static char caracter;
     
-    public void Linia(){
-        numeroCaracteres = 0;
+    
+    //MÉTODOS
+    
+    //Métodos Constructores
+    public Linia() {
+        numeroCaracteres=0;
     }
+    
+    //Métodos funcionales
+    //método que lleva a cabo la verificación de si una objeto Linea ha sido
+    //totalmente leido desde el teclado
+    public static boolean hayLinea() throws Exception {
+        //lectura primer caracter de la secuencia de entrada por teclado
+        caracter=LT.readCharacterLine();
+        //verificar final de linea
+        return (caracter!=FINAL_LINEA);
+    }
+    
+    //método que lleva a cabo la lectura de una linea desde el teclado
+    public void lectura() throws Exception {
+        //inicialización de numeroCaracteres
+        numeroCaracteres=0;
+        while (caracter!=FINAL_LINEA) {
+            //almacenar el caracter leido en el array caracteres del objeto Linea
+            caracteres[numeroCaracteres]=caracter;
+            //incrementar numeroCaracteres
+            numeroCaracteres++;
+            //lectura del siguiente caracter de la secuencia
+            caracter=LT.readCharacterLine();    
+        }
+    }
+    
+    //método que lleva a cabo la conversión de un objeto Linea a String para su
+    //visualización en pantalla
 
-    public int getNumeroCaracteres() {
-        return numeroCaracteres;
-    }
-    
-    public boolean vacia(){
-        return numeroCaracteres==0;
-    }    
-    public void Linia(String linia){
-        caracteres = linia.toCharArray();
-        numeroCaracteres = linia.length();
-    }
-    
-    public char obtenerCaracter(int posicion){
-        return caracteres[posicion];
-    }
-    
-    public void añadirCaracter(int codigo){
-        caracteres[numeroCaracteres]= (char)codigo;
-        numeroCaracteres++;
-    }
     @Override
     public String toString(){
-        String salida = "";
-        for(int  i = 0; i<numeroCaracteres; i++){
-            salida = salida + caracteres[i];
+        String salida="";
+        for (int indice=0;indice<numeroCaracteres;indice++) {
+            salida=salida+caracteres[indice];
         }
         return salida;
     }
+          
+    //método que lleva a cabo la adición de un caracter dado por parámetro
+    //al objeto Linea
+    public void adicionCaracter(int cod) {
+        caracteres[numeroCaracteres]=(char)cod;
+        numeroCaracteres++;
+    }
     
+    //método que devuelve el número de caracteres que conforman un objeto Linea
+    public int getNumCaracteres() {
+        return numeroCaracteres;
+    }
+    
+    //método que lleva a cabo la obtención del caracter del objeto Linea en función
+    //de la posición dada por parámetro
+    public char obtenerCaracter(int pos) {
+        return caracteres[pos];
+    }
+    
+    
+    //MÉTODO vacia QUE TIENE COMO OBJETIVO EL DE VERIFICAR SI UN OBJETO Linea 
+    //ESTÁ VACIO O NO
+    public boolean vacia() {
+        return (numeroCaracteres==0);
+    }
 }
